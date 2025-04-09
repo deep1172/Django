92360b3d7b0dd26c063e305313e9e052b58eb707
Deepak Kumar <1172dipu@gmail.com>
2025-04-09 13:45:39 +0530

    feat: Handle model and urls

--- Djangodev/deep/urls.py
+++ Djangodev/deep/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
    #localhost:8000/deep
    #localhost:8000/deep/order
     path('', views.all_deep , name='all_deep'),
+    path('/<int:deep_id>/', views.deep_detail , name='deep_detail'),
    
     path('services/', views.services, name='services' ),
    
--- Djangodev/deep/views.py
+++ Djangodev/deep/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import ChaiVariety
 
 # Create your views here.
 def all_deep(request):
-    return render(request, 'deep/all_deep.html')
+    deep = ChaiVariety.objects.all()
+    return render(request, 'deep/all_deep.html',{'deep': deep})
 
+def deep_detail(request, deep_id):
+    deep = get_object_or_404(ChaiVariety, pk=deep_id)
+    return render(request, 'deep/deep_detail.html',{'deep': deep})
 def services(request):
     return render(request, 'deep/services.html')
 
